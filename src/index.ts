dedb9170c8e3a065f549dba5f6c743cc86fba782
Andre Staltz <andre@staltz.com>
2016-11-21 11:32:47 +0200

    Bundle fonts in this repo too, not from Google Fonts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {run} from '@cycle/xstream-run';
 import {makeDOMDriver} from '@cycle/dom';
 import App from './App/index';
 import onionify from 'cycle-onionify';
+import './styles';
 
 const main = onionify(App);
 
---
+++ src/styles.ts
@@ -0,0 +1,13 @@
+import {cssRule} from 'typestyle';
+
+cssRule('@font-face', {
+  fontFamily: '"Source Sans Pro"',
+  src: 'url("./fonts/SourceSansPro-Regular.otf")',
+  fontWeight: 'normal',
+});
+
+cssRule('@font-face', {
+  fontFamily: '"Vesper Libre"',
+  src: 'url("./fonts/VesperLibre-Regular.ttf")',
+  fontWeight: 'normal',
+});
