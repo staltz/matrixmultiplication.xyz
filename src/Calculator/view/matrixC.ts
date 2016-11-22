2661d697bc3ea068504a31f5357c87e4a5a8e0ac
Andre Staltz <andre@staltz.com>
2016-11-22 16:09:20 +0200

    Implement transition for calculated cells of matrixC

--- src/Calculator/view/matrixC.ts
+++ src/Calculator/view/matrixC.ts
@@ -2,6 +2,34 @@ import {div, VNode} from '@cycle/dom';
 import {State} from '../model';
 import styles from '../styles';
 
+function mutateCellsStyle(state: State) {
+  const isCombing = (state.step === 1 && state.canInteract) || state.step > 1;
+
+  return function updateHook(prev: VNode, next: VNode) {
+    const all = (next.elm as Element).querySelectorAll('.cell');
+    for (let i = 0, N = all.length; i < N; i++) {
+      const cellElem = all.item(i) as HTMLElement;
+      const rowOfCell: number = parseInt((cellElem.dataset as any).row);
+      const colOfCell: number = parseInt((cellElem.dataset as any).col);
+      if (isCombing) {
+        cellElem.classList.add(styles.animatedCell);
+      } else {
+        cellElem.classList.remove(styles.animatedCell);
+      }
+      if (rowOfCell + colOfCell > state.step - 2) {
+        cellElem.style.color = null;
+        cellElem.style.opacity = '0.01';
+      } else if (rowOfCell + colOfCell === state.step - 2) {
+        cellElem.style.color = styles.colorPallete.blue;
+        cellElem.style.opacity = '1';
+      } else {
+        cellElem.style.color = null;
+        cellElem.style.opacity = '1';
+      }
+    }
+  }
+}
+
 export function maybeRenderMatrixC(matrixC: VNode | null, state: State): VNode | null {
   if (matrixC === null || (state.step <= 1 && !state.canInteract)) {
     return div(`.matrixC.${styles.resultMatrix}`, {style: { opacity: '0' }});
@@ -15,7 +43,10 @@ export function maybeRenderMatrixC(matrixC: VNode | null, state: State): VNode |
       style: {
         transform: `translateX(-${xDist}px) translateY(-${yDist}px)`,
         opacity: '1',
-      }
+      },
+      hook: {
+        update: mutateCellsStyle(state),
+      },
     }, [
       matrixC
     ]);
