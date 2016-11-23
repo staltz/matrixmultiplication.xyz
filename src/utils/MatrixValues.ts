b2953ef7a053cf03f45dc141732f5632cdbedc60
Andre Staltz <andre@staltz.com>
2016-11-23 16:24:12 +0200

    Refactor MatrixValues

--- src/utils/MatrixValues.ts
+++ src/utils/MatrixValues.ts
@@ -12,7 +12,7 @@ export default class MatrixValues {
   }
 
   clone(): MatrixValues {
-    let mv = new MatrixValues();
+    const mv = new MatrixValues();
     mv.numRows = this.numRows;
     mv.numCols = this.numCols;
     mv.values = this.values;
@@ -20,7 +20,7 @@ export default class MatrixValues {
   }
 
   static ofDimensions(rows: number, columns: number): MatrixValues {
-    let mv = new MatrixValues();
+    const mv = new MatrixValues();
     mv.numRows = Math.max(1, rows);
     mv.numCols = Math.max(1, columns);
     mv.values = makeValues(rows, columns);
@@ -28,7 +28,7 @@ export default class MatrixValues {
   }
 
   static from(vals: Array<Array<number>>): MatrixValues {
-    let mv = new MatrixValues();
+    const mv = new MatrixValues();
     mv.numRows = vals.length;
     mv.numCols = vals[0].length;
     mv.values = fromJS(vals);
@@ -36,12 +36,12 @@ export default class MatrixValues {
   }
 
   resize(numRows: number, numColumns: number): MatrixValues {
-    let {numRows: oldNumRows, numCols: oldNumCols} = this;
-    let nR = Math.max(1, numRows);
-    let nC = Math.max(1, numColumns);
+    const {numRows: oldNumRows, numCols: oldNumCols} = this;
+    const nR = Math.max(1, numRows);
+    const nC = Math.max(1, numColumns);
 
     // Make a new MatrixValues
-    let mv = new MatrixValues();
+    const mv = new MatrixValues();
     mv.numRows = nR;
     mv.numCols = nC;
 
@@ -62,7 +62,7 @@ export default class MatrixValues {
   }
 
   set(rowIndex: number, colIndex: number, value: number): MatrixValues {
-    let mv = this.clone();
+    const mv = this.clone();
     mv.values = mv.values.setIn([rowIndex, colIndex], value);
     return mv;
   }
@@ -72,7 +72,7 @@ export default class MatrixValues {
   }
 
   setAll(value: number | null): MatrixValues {
-    let mv = new MatrixValues();
+    const mv = new MatrixValues();
     mv.numRows = this.numRows;
     mv.numCols = this.numCols;
     mv.values = makeValues(mv.numRows, mv.numCols, value);
@@ -93,7 +93,7 @@ export default class MatrixValues {
 }
 
 function makeValues(numRows: number, numColumns: number, val: number | null = 1): List<List<number | null>> {
-  let vals = new Array<List<number | null>>(numRows);
+  const vals = new Array<List<number | null>>(numRows);
   for (let i = 0; i < numRows; i++) {
     vals[i] = makeRow(numColumns, val);
   }
@@ -101,7 +101,7 @@ function makeValues(numRows: number, numColumns: number, val: number | null = 1)
 }
 
 function makeRow(numColumns: number, val: number | null = 1): List<number | null> {
-  let row = new Array<number | null>(numColumns);
+  const row = new Array<number | null>(numColumns);
   for (let i = 0; i < numColumns; i++) {
     row[i] = val;
   }
