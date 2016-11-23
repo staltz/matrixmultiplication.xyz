0ad70ff8d58cfaac8ae002859ddfb2a061678df7
Andre Staltz <andre@staltz.com>
2016-11-23 16:21:34 +0200

    Refactor Calculator/index

--- src/Calculator/index.ts
+++ src/Calculator/index.ts
@@ -27,8 +27,8 @@ export default function Calculator(sources: Sources): Sinks {
   const cSinks = isolate(Matrix, 'matrixC')(<any> sources);
 
   const state$ = sources.onion.state$;
-  const action$ = xs.merge(intent(sources.DOM), timers(state$));
   const measurements$ = measure(sources.DOM);
+  const action$ = xs.merge(intent(sources.DOM), timers(state$));
   const reducer$ = model(action$, measurements$);
   const allReducer$ = xs.merge(reducer$, aSinks.onion, bSinks.onion);
   const vdom$ = view(state$, aSinks.DOM, bSinks.DOM, cSinks.DOM);
